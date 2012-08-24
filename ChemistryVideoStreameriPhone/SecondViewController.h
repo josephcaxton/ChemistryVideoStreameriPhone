eb56115515673f4864379bfed3bb5713723dc439
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-08-24 10:30:48 +0100

    Initial Commit

---
+++ ChemistryVideoStreameriPhone/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ChemistryVideoStreameriPhone
+//
+//  Created by Joseph caxton-Idowu on 24/08/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
